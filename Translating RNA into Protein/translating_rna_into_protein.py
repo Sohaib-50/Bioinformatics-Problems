943200b5ab032d244dec8dbc9e705b4387457592
Sohaib-50 <sohaibahmedabbasi0@gmail.com>
2022-12-16 22:24:57 +0500

    Translating RNA into Protein added

--- Translating RNA into Protein/translating_rna_into_protein.py
+++ Translating RNA into Protein/translating_rna_into_protein.py
@@ -1,9 +1,25 @@
 def RNA_to_protein(RNA):
-    pass
+    codon_table = {}
+    with open('codon_table.txt', 'r') as f:
+        for line in f:
+            line = line.strip().split()
+            for i in range(0, len(line), 2):
+                codon_table[line[i]] = line[i + 1]
+
+    protein = []
+    for i in range(0, len(RNA), 3):
+        codon = RNA[i : i + 3]  # pick 3 residues
+        amino_acid = codon_table[codon]
+        if amino_acid == "Stop":
+            break
+        protein.append(amino_acid)
+    
+    return "".join(protein)
 
 
-with open('.txt', 'r') as f:
+with open('rosalind_prot.txt', 'r') as f:
     RNA = f.read().strip()
 
+
 ans = RNA_to_protein(RNA)   
-print(ans)
+print(f"{ans}")
